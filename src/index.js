1f89ffd584e8f604fd0249a12f3fc0ebe24325a0
gubariev-vitalii <100084830+GubarievVitalii@users.noreply.github.com>
2022-06-12 20:54:51 +0300

    correct modal

---
+++ src/index.js
@@ -0,0 +1,29 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[open-locations]"),
+    closeModalBtn: document.querySelector("[close-locations]"),
+    modal: document.querySelector("[data-modal-locations]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[open-franchise]"),
+    closeModalBtn: document.querySelector("[close-franchise]"),
+    modal: document.querySelector("[data-modal-franchise]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
